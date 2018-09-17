6e7112adf7c094bcad184bc3d6737ca10476c4e0
Kigun6651(Laptop) <kigun6651@gmail.com>
2018-09-17 23:32:39 +0900

    Drill05 move to coordinate 7

--- Drills/Drill05/Drill05.py
+++ Drills/Drill05/Drill05.py
@@ -121,7 +121,22 @@ def move_to_coordinate_06():
 
 
 def move_to_coordinate_07():
-    pass
+    frame = 0
+    count = 0
+    x = 510
+    y = 92
+    move_shortest_x = (692 - 510) / 100
+    move_shortest_y = (518 - 92) / 100
+    while count <= 100:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 100, 100, 100, x, y)
+        update_canvas()
+        count += 1
+        x += move_shortest_x
+        y += move_shortest_y
+        frame = (frame + 1) % 8
+        delay(0.05)
 
 
 def move_to_coordinate_08():
@@ -142,7 +157,7 @@ def move():
     # move_to_coordinate_03()
     # move_to_coordinate_04()
     # move_to_coordinate_05()
-    move_to_coordinate_06()
+    # move_to_coordinate_06()
     move_to_coordinate_07()
     move_to_coordinate_08()
     move_to_coordinate_09()
