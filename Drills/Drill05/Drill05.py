581bc74b86c6e931a3c38ccbc30ee2c826a628b9
kigun6651(Laptop) <kigun6651@gmail.com>
2018-09-17 17:02:57 +0900

    Drill05 Whole Frame

--- Drills/Drill05/Drill05.py
+++ Drills/Drill05/Drill05.py
@@ -6,84 +6,62 @@ grass = load_image('grass.png')
 character = load_image('character.png')
 
 
-def move_from_center_to_right():
-    x, y = 800 // 2, 90
-    while x < 800 - 25:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        x += 2
-        delay(0.01)
+def move_to_coordinate_01():
+    pass
 
 
-def move_up():
-    x, y = 800 - 25, 90
-    while y < 600 - 50:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        y += 2
-        delay(0.01)
+def move_to_coordinate_02():
+    pass
 
 
-def move_left():
-    x, y = 800 - 25, 600 - 50
-    while x > 0 + 25:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        x -= 2
-        delay(0.01)
+def move_to_coordinate_03():
+    pass
 
 
-def move_down():
-    x, y = 0 + 25, 600 - 50
-    while y > 90:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        y -= 2
-        delay(0.01)
+def move_to_coordinate_04():
+    pass
 
 
-def move_from_left_to_center():
-    x, y = 0 + 25, 90
-    while x < 800 // 2:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        x += 2
-        delay(0.01)
+def move_to_coordinate_05():
+    pass
 
 
-def make_rectangle():
-    move_from_center_to_right()
-    move_up()
-    move_left()
-    move_down()
-    move_from_left_to_center()
+def move_to_coordinate_06():
+    pass
 
 
-import math
+def move_to_coordinate_07():
+    pass
 
 
-def make_circle():
-    cx, cy, r = 800 // 2, 600 // 2, (600 - 180)//2
-    degree = -90
-    while degree < 270:
-        radian = math.radians(degree)
-        x = cx + r * math.cos(radian)
-        y = cy + r * math.sin(radian)
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        degree += 1
-        delay(0.01)
+def move_to_coordinate_08():
+    pass
+
+
+def move_to_coordinate_09():
+    pass
+
+
+def move_to_coordinate_10():
+    pass
+
+
+def move():
+    move_to_coordinate_01()
+    move_to_coordinate_02()
+    move_to_coordinate_03()
+    move_to_coordinate_04()
+    move_to_coordinate_05()
+    move_to_coordinate_06()
+    move_to_coordinate_07()
+    move_to_coordinate_08()
+    move_to_coordinate_09()
+    move_to_coordinate_10()
 
 
 while True:
-    make_rectangle()
-    make_circle()
+    move()
+
 
     
 close_canvas()
