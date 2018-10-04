d56cbeef83f9a1b63ed8bc719f65c7af4529652a
Clutch06(Laptop) <kigun6651@gmail.com>
2018-10-04 17:11:25 +0900

    Drill#8-1 complete
    2017182025 윤인기
    커밋횟수 : 2회
    https://youtu.be/dqOhs3ew8AI

--- Drills/Drill08/Drill08-2.py
+++ Drills/Drill08/Drill08-2.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import random
 
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
@@ -7,11 +8,33 @@ kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 
 
-def move():
-    pass
+def move(p1, p2):
+    frame = 0
+    dir = 0
 
+    if p1[0] <= p2[0]:
+        dir = 1
+
+    if p1[0] > p2[0]:
+        dir = 0
+
+    for i in range(0, 100 + 1, 2):
+        clear_canvas()
+        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+        t = i / 100
+        x = (1 - t) * p1[0] + t * p2[0]
+        y = (1 - t) * p1[1] + t * p2[1]
+        character.clip_draw(frame * 100, dir * 100, 100, 100, x + 600, y + 400)
+        update_canvas()
+        frame = (frame + 1) % 8
+        delay(0.05)
+
+size = 10
+random_points = [(random.randint(-600, 600), random.randint(-400, 400)) for i in range(size)]
+n = 1
 
 while True:
-    move()
+    move(random_points[n - 1], random_points[n])
+    n = (n + 1) % size
 
 close_canvas()
