4d8494993edcc0d2b0012c9dfec5850b977ebfc0
Clutch06 (Desktop) <kigun6651@gmail.com>
2018-10-11 21:01:05 +0900

    Drill#10-1 complete
    2017182025 윤인기
    커밋횟수 : 4회
    https://youtu.be/US2JBDOC-FQ

--- Drills/Drill10/pause_state.py
+++ Drills/Drill10/pause_state.py
@@ -16,16 +16,28 @@ def exit():
     del(image)
 
 
-def handle_event():
+def handle_events():
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
         elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_p):
-            game_framework.pop_state()
+                game_framework.pop_state()
 
 
 def draw():
     clear_canvas()
     image.draw(400, 300)
     update_canvas()
+
+
+def update():
+    pass
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
